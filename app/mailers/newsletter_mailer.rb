0669f0cfde5b6c2490eb0e15c01f9ccdedea4d29
Adrian Sarstedt <adrian.sarstedt@gmail.com>
2017-08-01 15:00:13 +1000

    Add Newsletter Groundwork

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,7 +1,7 @@
 class NewsletterMailer < ActionMailer::Base
   default from: "social@melbourneunibasketball.org.au"
 
-  def distribute(newsletter_id, title)
+  def distribute(newsletter_id, title, extra_recipients)
     #still need to define newsletter object
     mail(to: @user.email, subject: 'MUBC Newsletter ' + newletter.name)
   end
