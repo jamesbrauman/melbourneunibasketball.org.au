0234a1fa390ddf62028961c826470e1bdfcc2a7d
Jason Kotchoff <jason@geoplus.com.au>
2017-09-03 22:34:36 +1000

    Fixed admin::Newsletters:create action

--- app/controllers/admin/newsletters_controller.rb
+++ app/controllers/admin/newsletters_controller.rb
@@ -7,18 +7,38 @@ class Admin::NewslettersController < Admin::BaseController
     @newsletter = Newsletter.new
   end
 
+  def show
+    @newsletter = Newsletter.find params[:id]
+  end
+
+
   def index
 
   end
 
   def create
-    Newsletter.create(title: "params[:title]")
-    puts "//////////////Newsletter Created///////////"
-    puts params
+    @newsletter = Newsletter.new(newsletter_params)
+
+    respond_to do |format|
+      if @newsletter.save
+debugger
+        format.html { redirect_to [:admin, @newsletter], notice: 'Newsletter was successfully created.' }
+        format.json { render json: @newsletter, status: :created, location: @newsletter }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @newsletter.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def deliver
     NewsletterMailer.distribute(params[:id], params[:title], params[:extra_recipients]).deliver
     redirect_to admin_newsletters_path, flash: {notice: "Newsletter was successfully delivered."}
+  end
+
+  private
+  def newsletter_params
+#    params[:newsletter].only(:title, :author, :content)
+    params[:newsletter]
   end
 end
