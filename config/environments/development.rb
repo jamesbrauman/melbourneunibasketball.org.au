3e16d7bfd257430a1ee8909953734e7c312ce857
James Brauman <james.22au@gmail.com>
2019-04-19 23:15:38 +1000

    fixup! Convert to use bootstrap 4.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # Adds support for live reload to rack.
-  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 end
